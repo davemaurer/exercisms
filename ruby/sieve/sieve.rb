ba522bbe7c278f5eb86b4ed76298a5621aedaa61
David Maurer <davemaurermail@gmail.com>
2017-08-19 20:22:08 -0600

    refactor sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -4,16 +4,14 @@ class Sieve
   end
 
   def number_range(number)
-    return nil if number < 2
+    return [] if number < 2
     (2..number).to_a
   end
 
   def primes
-    return [] if @numbers.nil?
     max = @numbers[-1]
     @numbers.each do |num|
-      next unless num
-      break if num * num > max
+      next unless num && !(num * num > max)
       (num * num).step(max, num) do |n|
         @numbers[@numbers.index(n)] = nil if @numbers.index(n)
       end
