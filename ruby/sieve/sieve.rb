46fb4109f59bd813d98745ee69903cfd43b848f2
David Maurer <davemaurermail@gmail.com>
2017-08-19 19:49:14 -0600

    sieve all tests passing.

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -4,12 +4,24 @@ class Sieve
   end
 
   def number_range(number)
-    return nil if number <= 2
-    (2..number)
+    return nil if number < 2
+    (2..number).to_a
   end
 
   def primes
-    result = []
-    return result if @numbers.nil?
+    return [] if @numbers.nil?
+    max = @numbers[-1]
+    @numbers.each do |num|
+      next unless num
+      break if num * num > max
+      (num * num).step(max, num) do |n|
+        @numbers[@numbers.index(n)] = nil if @numbers.index(n)
+      end
+    end
+    @numbers.compact
   end
+end
+
+module BookKeeping
+  VERSION = 1
 end
