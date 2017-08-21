cb978884b9701e4fc6a2f2c2a6e9e6cc72b8b68f
David Maurer <davemaurermail@gmail.com>
2017-08-21 12:45:50 -0600

    account for 9s in roman numerals.

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,8 +1,8 @@
 class Fixnum
   NUMERAL_VALUES = {
     'M' => 1000, 'D' => 500, 'C' => 100,
-    'L' => 50, 'X' => 10, 'V' => 5, 'IV' => 4,
-    'I' => 1
+    'L' => 50, 'X' => 10, 'IX' => 9, 'V' => 5,
+    'IV' => 4, 'I' => 1
   }
 
   def to_roman
